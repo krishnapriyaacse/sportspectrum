d94a4945d5cf5380e54bad2c756efce89d6e10bd
krishnapriyaacse <krishnapriyaa.cse2024@citchennai.net>
2025-08-21 14:41:48 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SortSpectrum</h1>
+      <p>Algorithm visualization will go here.</p>
+    </div>
+  );
+}
+
+export default App;
